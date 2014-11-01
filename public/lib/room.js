324c8d46591288e2e37a383ed74717c3b20a52e5
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2014-11-01 16:02:46 +0900

    fix start button

--- public/lib/room.js
+++ public/lib/room.js
@@ -12,10 +12,16 @@ function Room(selector) {
   this.$users = this.$node.find('.users');
 
   this.chat = new Chat(this.$node.find('.chat'));
+  this.room = null;
 
   var self = this;
 
+  this.$startGame.click(function() {
+    socket.emit('start game');
+  });
+
   socket.on('join room', function(room) {
+    self.room = room;
     self.chat.refresh();
     self.chat.log('You have joined ' + room.name);
 
@@ -33,23 +39,37 @@ function Room(selector) {
   });
 
   socket.on('leave room', function(room) {
+    self.room = null;
     self.$node.hide();
   });
 
   socket.on('user joined', function(user) {
+    self.room.users.push(user);
     self.chat.log(user.username + ' joined');
     self.$users.append(createUserNode(user));
+    self.$startGame.prop('disabled', !self.startable());
   });
 
   socket.on('user left', function(user) {
+    self.room.users.some(function(u, i) {
+      if (user.id !== u.id) return;
+      self.room.users.splice(i, 1);
+      return true;
+    });
+
     self.chat.log(user.username + ' left');
     self.$users.find('.user[data-id=' + user.id + ']').remove();
+    self.$startGame.prop('disabled', !self.startable());
   });
 
   socket.on('room closed', function(room) {
     alert('The game was closed');
     self.$node.hide();
   });
+}
+
+Room.prototype.startable = function() {
+  return !!(this.room && this.room.users.length > 1);
 }
 
 function createUserNode(user) {
